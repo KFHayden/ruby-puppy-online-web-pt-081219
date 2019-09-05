8729aa17f58b7d96977a9a977d2f2be2887f6a7e
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-05 23:13:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  @@clear_all = @@all
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@clear_all.clear
+  end
+  self
+  
+end
